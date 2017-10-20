625dc5c8f73e6c9e7d131c073b4e3c9d98873799
LukaszKur <lukaszkurasz2@gmail.com>
2017-10-20 11:13:05 +0200

    Mnozenie i dodawanie

---
+++ program.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int mnoz(int a,int b) 
+	{
+	return a*b;
+	}
+int dodaj(int a, int b)
+	{
+	return a+b;
+	}
+
+int main(void)
+{
+	int a=2, b=5;
+	char wybor='*';
+	if(wybor =='*')
+	{
+	printf("Wynik mnozenia: %i\n", mnoz(a,b));
+	}
+	else
+	{
+	printf("Wynik dodawania: %i\n", dodaj(a,b));
+	};
+	return 0;
+}
+
