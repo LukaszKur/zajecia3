eba270a9203504172c8164fc5d0346939fcc65f3
LukaszKur <lukaszkurasz2@gmail.com>
2017-10-20 13:59:59 +0200

    Kalkulator

--- program.c
+++ program.c
@@ -7,10 +7,24 @@ int dodaj(int a, int b)
 	{
 	return a+b;
 	}
-
+int odejmij(int a, int b)
+	{
+	return a-b;
+	}
+int dziel(int a, int b)
+	{
+	if (b==0)
+	{
+	printf("Nie dziel cholero przez zero :)\n");
+	}
+	else
+	{
+	return a/b;
+	}
+	}
 int main(void)
 {
-	int a=2, b=5;
+	int a=5, b=2;
 	char wybor;
 	scanf("%c", &wybor);
 	if(wybor =='*')
@@ -19,8 +33,29 @@ int main(void)
 	}
 	else
 	{
-	printf("Wynik dodawania: %i\n", dodaj(a,b));
+		if(wybor =='+')
+		{
+		printf("Wynik dodawania: %i\n", dodaj(a,b));
+		}
+		else
+		{
+			if(wybor =='-')
+			{
+			printf("Wynik odejmowania: %i\n",odejmij(a,b));
+			}
+			else
+			{
+				if(wybor =='/')
+                        		{
+                        		printf("Wynik dzielenia: %i\n",dziel(a,b));
+					}
+				else
+				{
+				printf("Dokonano niewlasciwego wyboru! \n");
+				}
+			}
+		}
 	};
-	return 0;
+return 0;
 }
 
